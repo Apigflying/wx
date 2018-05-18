9c7319d5652a1be065199f056c65a3c8a9f89643
Apigflying <809276596@qq.com>
2018-05-18 14:35:30 +0800

    修改wxmusic

--- wxmusic/components/content/content.js
+++ wxmusic/components/content/content.js
@@ -17,13 +17,26 @@ Component({
    * 组件的初始数据
    */
   data: {
-    
+    scroll: true,
+    selectedId: '11',
+    list: [{
+      id: '11',
+      title: '第一页'
+    }, {
+      id: '21',
+      title: '第二页'
+    }, {
+      id: '31',
+      title: '第三页'
+    }]
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    handleTabChange(e) {
+      console.log(e.detail);
+    },
   }
 })
