31df1e325823a9b2b2f1b47a1dcd48c3864de366
Apigflying <809276596@qq.com>
2018-05-22 17:51:23 +0800

    完善 音乐库 - 推荐页面

--- wxmusic/app.js
+++ wxmusic/app.js
@@ -2,14 +2,9 @@
 
 App({
   onShow(){
-    console.log('切换到前台时的钩子函数');
-    console.log('切换到前台时的钩子函数');
-    console.log('切换到前台时的钩子函数');
-    console.log('切换到前台时的钩子函数');
-    console.log('切换到前台时的钩子函数');
-    
+
   },
   onHide(){
-    console.log('从前台切换到后台的钩子函数')
+    
   }
 })
--- wxmusic/components/main/library/library.js
+++ wxmusic/components/main/library/library.js
@@ -1,4 +1,5 @@
-// components/main/library/library.js
+const { baseUrl } = require('../../../utils/base.js');
+console.log(baseUrl);
 Component({
   properties: {
 
@@ -20,7 +21,7 @@ Component({
     swiper: {
       swiperList: [],
       current: 0, // 当前展示的
-      autoplay: false,// 是否自动轮播
+      autoplay: true,// 是否自动轮播
       circular: true, // 是否采用衔接滑动
       idots: true, // 是否展示面板指示点
       dotColor: 'rgba(255,255,255,0.4)',// 面板指示点默认颜色
@@ -28,13 +29,37 @@ Component({
     },
 
   },
+  // 钩子函数
+  ready() {
+    // 获取推荐的banner
+    this.getBanners();
+  },
   methods: {
+    //========事件========\\
+    // 点击图片事件
+    toThePage(e){
+      console.log(e);
+    },
+    // 二级导航切换事件
     tabChange(e) {
       let selectedId = e.detail;
       this.setData({
         selectedId
       });
     },
-    
+    //========API=========\\
+    getBanners() {
+      wx.request({
+        url: `${baseUrl}/banner`,
+        success:(res)=>{
+          let { data: { banners } } = res;
+          let {swiper} = this.data;
+          swiper.swiperList = banners;
+          this.setData({
+            swiper
+          });
+        }
+      })
+    },
   }
 })
